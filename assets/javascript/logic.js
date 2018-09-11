f241bc0e8bee95e745aa6cc006a96b49a63cf953
Chancy <chancyleath@hotmail.com>
2018-09-11 09:41:52 -0600

    final commit before ttrying to add additional items

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -38,7 +38,6 @@ $(document).ready(function() {
         var trainName = $("#trainName").val().trim();
         var destination = $("#destination").val().trim();
         //add moment js formatting to ensure in military time HH:mm
-        //var firstTrainTime = $("#firstTrainTime").val().trim();
         var firstTrainTime = moment($("#firstTrainTime").val().trim(), "HH:mm").format("HH:mm");
         var frequency = $("#frequency").val().trim();
 
@@ -50,70 +49,65 @@ $(document).ready(function() {
             frequency: frequency
         };
 
-        //testing
-        console.log("train time input: " + trainName);
-        console.log("this is destination input: " +destination);
-        console.log("this is the first train time input: " + firstTrainTime);
-        console.log("thhi is the frequency input: " + frequency);
-
-        //database.ref().push object for new train info to create a child node
+        //push the object for new train info to the DB to create a child node
         database.ref().push(newTrainInfo);
 
         //alert new train added successfully (change to boostrap modal later?)
-        alert("New Train Add Successfully");
+        alert("New Train Added Successfully");
 
-        //clear inputs after submit
+        //clear inputs after submit is hit
         $("#trainName").val("");
         $("#destination").val("");
         $("#firstTrainTime").val("");
         $("#frequency").val("");
+
+        //testing
+        // console.log("train time input: " + trainName);
+        // console.log("this is destination input: " + destination);
+        // console.log("this is the first train time input: " + firstTrainTime);
+        // console.log("thhi is the frequency input: " + frequency);
     });
 
-    //on value or on child event for datbase to be call
+    //on child event for datbase to be call to get the snapshot values
     database.ref().on("child_added", function(snapshotChild) {
         //create a variable to hold the child value
         var cv = snapshotChild.val();
+
         //assign the child snapshot values to the  variables
         trainName = cv.name;
         destination = cv.destination;
-        //var firstTrainTime = "09:00";
-        // firstTrainTime = moment(cv.firstTrainTime).format("HH:mm");
         firstTrainTime = cv.firstTime;
         frequency = cv.frequency;
 
         //testing section
         console.log(trainName);
         console.log(destination);
-        //NOTE: its saying the first train time is undefined...why??
         console.log("first train time " + firstTrainTime);
         console.log("frequency " + frequency);
 
-        //convert the first time to 1 year ago to prevent issues
-        var firstTrainTimeConv = moment(firstTrainTime, "HH:mm").subtract(1, "years");
-            console.log("first train time converted " + firstTrainTimeConv);
+        //convert the first time to 1 year ago to prevent issues (and make sure its in military time)
+        var firstTrainTimeConv = moment(firstTrainTime, "HH:mm").subtract(1, "years"); 
 
-        //take the difference from the first time and the current time (current time is moment())
-        var currentTime = moment()
-            console.log("current time: " + moment(currentTime).format("HH:mm"));
-
-        //get the time difference in minutes
-        //NOTE: This is ALWAYS zero, I think because of the first train time being undefined
-        var timeDiff = currentTime.diff(moment(firstTrainTimeConv), "minutes");
-            console.log("time difference " + timeDiff);
-
-        //this is the remaining time b
+        //get the time difference from the current time (which is just "moment()") to the first trains arrival in minutes
+        var timeDiff = moment().diff(moment(firstTrainTimeConv), "minutes");
+    
+        //take the modulus of the time difference and the frequency (this is the remaining time variable)
         var timeRemainder = timeDiff % frequency;
-            console.log("remaining time: " + timeRemainder);
 
-        //get the minutes to the next train
-        var minAway = frequency - timeRemainder;
-            console.log("minutes until next train: " + minAway);
+        //get the minutes to the next train by subtracting the remaining time variable from the frequency
+        var minAway = frequency - timeRemainder; 
 
-        //take the time from the first arrival to current time and assign to var for next arrival based on the frequency
-        var nextArrival = moment().add(minAway, "minutes");
-        //convert time into am/pm format using momentjs
-        var nextArrivalPretty = moment(nextArrival).format("hh:mm a");
-            console.log("This is the pretty time: " + nextArrivalPretty);
+        //take the time from now ("moment()") and add it to minutes away variable (in minutes format),
+        //assign it to var for next arrival,
+        //change format to normal looking time
+        var nextArrival = moment().add(minAway, "minutes").format("hh:mm A");
+
+        //time testing section
+        console.log("first train time converted " + firstTrainTimeConv);
+        console.log("time difference " + timeDiff);
+        console.log("remaining time: " + timeRemainder);
+        console.log("minutes until next train: " + minAway);
+        console.log("the next arrival " + nextArrival);
 
         //make a var for a new table row
         var newRow = $("<tr>").append(
@@ -121,7 +115,7 @@ $(document).ready(function() {
         $("<td>").text(trainName),
         $("<td>").text(destination),
         $("<td>").text(frequency),
-        $("<td>").text(nextArrivalPretty),
+        $("<td>").text(nextArrival),
         $("<td>").text(minAway)
         );
 
